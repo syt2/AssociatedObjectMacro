f328cf16e01d1a38cde34f4f11e9ce2e9b41c782
shenyutao <shenyutao@nutstore.net>
2023-06-25 16:36:36 +0800

    remove duplicate declaration

--- Sources/AssociatedObject/AssociatedObject.swift
+++ Sources/AssociatedObject/AssociatedObject.swift
@@ -2,8 +2,4 @@ import ObjectiveC
 
 @attached(peer, names: arbitrary)
 @attached(accessor)
-public macro AssociatedObject(policy: objc_AssociationPolicy, defaultValue: Any?) = #externalMacro(module: "AssociatedObjectMacros", type: "AssociatedObjectMacro")
-
-@attached(peer, names: arbitrary)
-@attached(accessor)
-public macro AssociatedObject(policy: objc_AssociationPolicy) = #externalMacro(module: "AssociatedObjectMacros", type: "AssociatedObjectMacro")
+public macro AssociatedObject(policy: objc_AssociationPolicy, defaultValue: Any? = nil) = #externalMacro(module: "AssociatedObjectMacros", type: "AssociatedObjectMacro")
